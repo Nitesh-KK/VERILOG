9159889f50f1b3a10d5b4a6e50920fa918479858
Nitesh-KK <niteshkk.github@gmail.com>
2026-01-06 22:58:28 +0530

    Update and rename 038. Binary to grey code converter.v to 038. Binary to gray code converter.v

---
+++ 3.Combinational_circuits_1/038. Binary to gray code converter.v
@@ -0,0 +1,9 @@
+module bin_to_gray(input [3:0] bin,output [3:0] gray);
+  assign gray[3]=bin[3];
+  genvar i;
+  generate
+    for(i=2;i>=0;i--) begin
+      assign gray[i]=bin[i]^bin[i+1];
+    end
+  endgenerate
+endmodule
--- 3.Combinational_circuits_1/038. Binary to grey code converter.v
+++
@@ -1,9 +0,0 @@
-module bin_to_grey(input [3:0] bin,output [3:0] grey);
-  assign grey[3]=bin[3];
-  genvar i;
-  generate
-    for(i=2;i>=0;i--) begin
-      assign grey[i]=bin[i]^bin[i+1];
-    end
-  endgenerate
-endmodule
