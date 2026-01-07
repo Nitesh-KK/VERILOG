03f03b7832f15f88d0e6474543b6f21097581dee
Nitesh-KK <niteshkk.github@gmail.com>
2026-01-07 22:48:04 +0530

    Update 038. Binary to gray code converter.v

--- 3.Combinational_circuits_1/038. Binary to gray code converter.v
+++ 3.Combinational_circuits_1/038. Binary to gray code converter.v
@@ -2,7 +2,7 @@ module bin_to_gray(input [3:0] bin,output [3:0] gray);
   assign gray[3]=bin[3];
   genvar i;
   generate
-    for(i=2;i>=0;i--) begin
+    for(i=0;i<3;i++) begin
       assign gray[i]=bin[i]^bin[i+1];
     end
   endgenerate
